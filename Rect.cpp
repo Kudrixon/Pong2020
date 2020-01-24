061b706efae18f18b2e212572654d11818bef11a
Kudrixon <35082391+Kudrixon@users.noreply.github.com>
2020-01-24 11:09:18 +0100

    Update Rect.cpp

--- Rect.cpp
+++ Rect.cpp
@@ -1,5 +1,5 @@
 #include "Rect.h"
-
+#include "Ball.h"
 Rect::Rect(Window &window, int wrect, int hrect, int xrect, int yrect, int r, int g, int b, int a) :
 	Window(window), _wrect(wrect), _hrect(hrect), _xrect(xrect), _yrect(yrect), _r(r), _g(g), _b(b), _a(a)
 {
@@ -27,13 +27,61 @@ void Rect::keyevent()
 		{
 			if (event.key.keysym.sym == SDLK_UP)
 			{
-				_yrect -= 5;
+				_yrect -= 10;
 			}
 			else if (event.key.keysym.sym == SDLK_DOWN)
 			{
-				_yrect += 5;
+				_yrect += 10;
 			}
 		}
+
 	}
 
+}
+
+void Rect::movement() 
+{
+	// KIERUNEK Y
+	if(_yrect == 599 && _yoff == 0)
+	{
+		_yoff = 1;
+	}
+	else if (_yrect == 1 && _yoff == 1)
+	{
+		_yoff = 0;
+	}
+	else if(_yoff == 0)
+	{
+		_yrect++;
+		SDL_Delay(10);
+	}
+	else if(_yoff == 1)
+	{
+		_yrect--;
+		SDL_Delay(10);
+	}
+	// KIERUNEK X
+	if (_xrect == 799 && _xoff == 0)
+	{
+		_xoff = 1;
+	}
+	else if (_xrect == 1 && _xoff == 1)
+	{
+		_xoff = 0;
+	}
+	else if (_xoff == 0)
+	{
+		_xrect++;
+	}
+	else if (_xoff == 1)
+	{
+		_xrect--;
+	}
+
+
+}
+
+int Rect::yer()
+{
+	return _yrect;
 }
