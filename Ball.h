0c80a380755da87199a35fb6a7f237bae4fd64cf
Kudrixon <35082391+Kudrixon@users.noreply.github.com>
2020-01-25 20:03:42 +0100

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -2,23 +2,24 @@
 
 #include "window.h"
 
-class Ball :public Window
+class Ball
 {
 public:
-	Ball(Window& window, int wrect, int hrect, int xrect, int yrect, int r, int g, int b, int a);
+	Ball(int wrect, int hrect, int xrect, int yrect, int r, int g, int b, int a);
 
 	void draw();
 	void movement();
+	void keyspeedup();
 	void speedup();
 	int xcollider();
 	int ycollider();
 	void resposition();
 	int _xrect, _yrect;
+	int _vel = 1;
 
 private:
 	int _wrect, _hrect;
 	int _r, _g, _b, _a;
 	int _yoff = 0;
 	int _xoff = 0;
-	int _vel = 1;
 };
