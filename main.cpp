2e2c0059bb523f3853a9011cd0d64388612b5abf
Kudrixon <35082391+Kudrixon@users.noreply.github.com>
2020-01-27 08:36:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "window.h"
 #include "Rect.h"
-#include "Ball.h"
-#include "Text2.h"
+#include "Ball2.h"
+#include "Text.h"
 #include <iostream>
 #include <SDL_ttf.h>
 #include <random>
 #include <ctime>
 
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 
 	std::string p1score; //Wynik startowy
@@ -18,6 +18,13 @@ int main(int argc, char **argv)
 	p1score = "0";
 	p2score = "0";
 
+	int aiFault = rand() % 20;//wyznacza omyslnosc przeciwnika
+	int counter = 0; //liczy ile razy sie odbiła piłka
+
+	int arr[180]; //tablica odpowiedzialna za sprawdzanie czy piłka zdrzeyła sie z rakietką
+	int arr2[180];
+
+	srand(time(NULL));
 
 	Window window("KudriPong", 800, 600);
 	Rect rect(40, 160, 10, 220, 255, 0, 0, 255);
@@ -25,24 +32,20 @@ int main(int argc, char **argv)
 	Rect line(5, 600, 396, 0, 255, 255, 255, 255);
 	Ball ball(20, 20, 390, 290, 200, 0, 200, 255);
 
-	srand(time(NULL));
-	int aiFault = rand() % 20;//wyznacza omyslnosc przeciwnika
 
-	int counter = 0; //liczy ile razy sie odbiła piłka
-
-	while (window.opened() && (tempscore1 < 11) && (tempscore2 < 11)) 
+	while (window.opened() && (tempscore1 < 11) && (tempscore2 < 11))
 	{
-		
+
 		Text text(Window::renderer, "Roboto-Black.ttf", 50, p1score, { 255, 255, 255, 255 });
 		Text text2(Window::renderer, "Roboto-Black.ttf", 50, p2score, { 255, 255, 255, 255 });
 
 		if (counter < aiFault) //odpowiedzialne za bledy przeciwnika
 		{
-			if( ball.ycollider() <= 65 || ball.ycollider() >= 505)
+			if (ball.ycollider() <= 65 || ball.ycollider() >= 505)
 			{
 
 			}
-			else 
+			else
 			{
 				rect2._yrect = (ball._yrect - 60);
 			}
@@ -53,15 +56,11 @@ int main(int argc, char **argv)
 		}
 
 
-		int arr[180]; //tablica odpowiedzialna za sprawdzanie czy piłka zdrzeyła sie z rakietką
-		int arr2[180];
-
-		for (int i = 0; i < 180; i++)
+		for (int i = 0; i < 180; i++) // wypelnianie tablic odpowiedzialnych za hitbox
 		{
 			arr[i] = (rect.yer() - 20) + i; // -20 bo y pilki ma 20
 			arr2[i] = (rect2.yer() - 20) + i;
 		}
-
 
 
 		bool existsp1 = std::find(std::begin(arr), std::end(arr), ball.ycollider()) != std::end(arr); //sprawdza czy wspolrzedna pilki znajduje sie w tablicy w ktorej są wspolrzedne paletki
@@ -69,7 +68,7 @@ int main(int argc, char **argv)
 
 
 		if (existsp1 && ball.xcollider() <= 50) // odpowiedzialne za przyznawanie pkt i reset jak przegrasz
-		{ 
+		{
 			std::cout << "Obroniles sie" << std::endl;
 			counter++;
 
@@ -94,7 +93,7 @@ int main(int argc, char **argv)
 			//ball.speedup(); <--------Tylko dla odwaznych
 		}
 
-		
+
 		text.show(350, 20, Window::renderer);
 		text2.show(420, 20, Window::renderer);
 		rect.draw();
@@ -108,6 +107,6 @@ int main(int argc, char **argv)
 		window.background(50, 50, 50); //podajemy kolor tła
 		window.buttonevent();
 	}
-	
+
 	return 0;
 }
